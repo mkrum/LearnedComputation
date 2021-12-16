4ece559da0c477c1795fc44900ba1a68da4928ad
Michael Krumdick <mrkrumdick@gmail.com>
2021-12-16 00:00:30 +0000

    Formatting

--- lc/plot.py
+++ lc/plot.py
@@ -2,19 +2,20 @@ from collections import namedtuple
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 sns.set()
 
 
 def load_data(log_file):
     lines = open(log_file).readlines()
 
-    test_data = filter(lambda x: 'test' in x, lines)
-    train_data = filter(lambda x: 'train' in x, lines)
-    
+    test_data = filter(lambda x: "test" in x, lines)
+    train_data = filter(lambda x: "train" in x, lines)
+
     train_log = namedtuple("TrainLog", ["timestamp", "epoch", "batch_idx", "loss"])
     train_logs = []
     for t in train_data:
-        timestamp,_,epoch,batch_idx,loss = t.split(',')
+        timestamp, _, epoch, batch_idx, loss = t.split(",")
 
         tl = train_log(float(timestamp), int(epoch), int(batch_idx), float(loss))
         train_logs.append(tl)
@@ -22,7 +23,7 @@ def load_data(log_file):
     test_log = namedtuple("TestLog", ["timestamp", "epoch", "mse", "valid", "acc"])
     test_logs = []
     for (epoch, t) in enumerate(test_data):
-        timestamp,_,mse,valid,acc = t.split(',')
+        timestamp, _, mse, valid, acc = t.split(",")
         tl = test_log(float(timestamp), epoch, float(mse), float(valid), float(acc))
         test_logs.append(tl)
 
@@ -37,6 +38,7 @@ def loss_plot(ax, base_timestamp, train_logs):
     ax.set_title("Training Loss")
     ax.set_ylabel("Negative Log Likelihood")
 
+
 def acc_and_valid_plot(ax, base_timestamp, test_logs):
     timestamps = list(map(lambda x: x.timestamp - base_timestamp, test_logs))
     accs = list(map(lambda x: 100 * x.acc, test_logs))
@@ -48,6 +50,7 @@ def acc_and_valid_plot(ax, base_timestamp, test_logs):
     ax.set_ylabel("Percentage")
     ax.legend(["Accuracy", "Validity"])
     ax.set_title("Success Metrics")
+
 
 def mse_plot(ax, base_timestamp, test_logs):
     timestamps = list(map(lambda x: x.timestamp - base_timestamp, test_logs))
@@ -69,6 +72,7 @@ def training_plot(log_file):
     mse_plot(ax3, base_timestamp, test_logs)
     ax3.set_xlabel("Time (s)")
 
-if __name__ == '__main__':
-    training_plot("example.log") 
+
+if __name__ == "__main__":
+    training_plot("example.log")
     plt.show()
